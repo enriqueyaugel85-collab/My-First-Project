4266cc45d3cad9b0fa6877052cfe7157ca8f8824
Enrique Yaugel <enriqueyaugel85@gmail.com>
2026-06-14 20:08:52 -0400

    Added gui to the password generator

--- Random Passwords generator.py
+++ Random Passwords generator.py
@@ -1,45 +1,128 @@
 import random
 import string
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
 
+root = tk.Tk()
+user_options = ["Yes", "No", "Submit"]
 
-password_leng = 0
 questions = ["Enter desired password length: ",
-             "Include uppercase letters? (y/n): ", "Include special letters? (y/n): ", "Include digits? (y/n): "]
+             "Include uppercase letters?", "Include special letters?", "Include digits?", "What app you want it for?", "Do you want to save the pasword?"]
+ques_num = 0
+password_leng = 0
+letters = ""
+letter_upper = ""
+special = ""
+digits = ""
+app = ""
 
 
-def setting_password(desire_question):
-    if desire_question == "letters_upper":
-        return string.ascii_uppercase if input(questions[1]).lower() == "y" else ""
-    elif desire_question == "special_char":
-        return string.punctuation if input(questions[2]).lower() == "y" else ""
-    elif desire_question == "digits":
-        return string.digits if input(questions[3]).lower() == "y" else ""
-
-
-def create_password():
+def create_password(current_question, user_ans):
     gen_password = ""
+    global password_leng
+    global letters
+    global letter_upper
+    global special
+    global digits
     letters = string.ascii_lowercase
-    try:
-        password_leng = int(input(questions[0]))
-    except (ValueError, TypeError):
-        print("You dint enter valid number setting length to 12")
-        password_leng = 12
-    letter_upper = setting_password("letters_upper")
-    digits = setting_password("digits")
-    special = setting_password("special_char")
-    while len(gen_password) < password_leng:
+    if current_question == 0:
+        password_leng = user_ans if user_ans else 12
+    if current_question == 1:
+        letter_upper = string.ascii_uppercase if user_ans == "yes" else ""
+    elif current_question == 2:
+        special = string.punctuation if user_ans == "yes" else ""
+    elif current_question == 3:
+        digits = string.digits if user_ans == "yes" else ""
+    elif current_question == 4:
+        app = user_ans if user_ans else "unkwon"
 
-        gen_password += random.choice(letters)
-        gen_password += random.choice(letter_upper)
-        gen_password += random.choice(digits)
+    elif current_question == 5:
 
-        gen_password += random.choice(special)
-    return gen_password
+        char = letters + letter_upper + special + digits
+        for i in range(password_leng):
+            gen_password += random.choice(char)
+        text_result.delete(1.0, "end")
+        text_result.insert(1.0, gen_password)
+    if current_question <= 4:
+        global ques_num
+        ques_num = current_question + 1
+        set_password_gui()
+        text_result.delete(1.0, "end")
+        text_result.insert(1.0, questions[ques_num])
 
 
-while True:
-    password = create_password()
-    print(f"These is your final password: {password}")
-    ask_user = input("You want to create another password? (y/n)")
-    if ask_user.lower() == "n":
-        break
+def submit():
+    global entry_answer
+    global ques_num
+    global btn_sub
+    if ques_num == 0:
+        try:
+            answer = int(entry_answer.get())
+            if answer > 8 and answer < 30:
+                text_result.delete(1.0, "end")
+                create_password(ques_num, answer)
+                set_password_gui()
+                btn_sub.grid_remove()
+                entry_answer.grid_remove()
+
+        except (ValueError):
+            pass
+    elif ques_num == 4:
+        text_result.delete(1.0, "end")
+        answer = str(entry_answer.get())
+        create_password(ques_num, answer)
+        btn_sub.grid_remove()
+        entry_answer.grid_remove()
+
+
+def set_password_gui():
+    global questions
+    global ques_num
+    global password_leng
+    global entry_answer
+    global btn_sub
+    global btn_no
+    global btn_yes
+    if ques_num == 0:
+        text_result.insert(1.0, questions[ques_num])
+    if ques_num == 1:
+
+        btn_yes.grid(row=2, column=2)
+
+        btn_no.grid(row=3, column=2)
+    if ques_num == 4:
+        btn_yes.grid_remove()
+        btn_no.grid_remove()
+        btn_sub.grid(row=3, column=0)
+        entry_answer.grid(row=2, column=0)
+    if ques_num == 5:
+        btn_yes.grid(row=2, column=2)
+        btn_no.grid(row=3, column=2)
+
+
+btn_yes = tk.Button(root, text=(user_options[0]), width=15, font=(
+    "Arial", 14), command=lambda: create_password(ques_num, "yes"))
+btn_no = tk.Button(root, text=(user_options[1]), width=15, font=(
+    "Arial", 14), command=lambda: create_password(ques_num, "no"))
+btn_sub = tk.Button(root, text=(user_options[2]), width=15, font=(
+    "Arial", 14), command=lambda: submit())
+text_result = tk.Text(root, height=5, width=34, font=("Arial", 24))
+text_result.grid(columnspan=5)
+text_result.configure(bg="gray34", fg="white", borderwidth=0)
+btn_sub.grid(row=3, column=0)
+entry_answer = tk.Entry(root, font=("Arial", 14))
+entry_answer.grid(row=2, column=0)
+root.geometry("600x300")
+root.title("Calculator")
+root.resizable(0, 0)
+root.configure(bg="gray24")
+set_password_gui()
+root.mainloop()
+
+# while True:
+# password = create_password()
+# print(f"These is your final password: {password}")
+# ask_user = input("You want to create another password? (y/n)")
+# if ask_user.lower() == "n":
+#    break
